d67abd34759969165a30fe7ef9c0b77d51156c62
Dmitry Konyshev <probables@emcraft.com>
2013-03-28 18:52:47 +0400

    minor code clean

--- board/emcraft/vybrid_som/vybrid_som.c
+++ board/emcraft/vybrid_som/vybrid_som.c
@@ -44,8 +44,6 @@ struct fsl_esdhc_cfg esdhc_cfg[2] = {
 };
 #endif
 
-#define reg32_write(a, b) __raw_writel(b, a)
-
 void setup_iomux_ddr(void)
 {
 #define DDR_INPUT	(0 << 16)
@@ -57,56 +55,56 @@ void setup_iomux_ddr(void)
 #define DDR_TRIM1	(0 << 14)
 #define DDR_DSE1	(5 << 6)
 #define DDR_IOMUX1	(DDR_INPUT1 | DDR_TRIM1 | DDR_DSE1)
-	reg32_write(0x4004821c, DDR_IOMUX);
-	reg32_write(0x40048220, DDR_IOMUX);
-	reg32_write(0x40048224, DDR_IOMUX);
-	reg32_write(0x40048228, DDR_IOMUX);
-	reg32_write(0x4004822c, DDR_IOMUX);
-	reg32_write(0x40048230, DDR_IOMUX);
-	reg32_write(0x40048234, DDR_IOMUX);
-	reg32_write(0x40048238, DDR_IOMUX);
-	reg32_write(0x4004823c, DDR_IOMUX);
-	reg32_write(0x40048240, DDR_IOMUX);
-	reg32_write(0x40048244, DDR_IOMUX);
-	reg32_write(0x40048248, DDR_IOMUX);
-	reg32_write(0x4004824c, DDR_IOMUX);
-	reg32_write(0x40048250, DDR_IOMUX);
-	reg32_write(0x40048254, DDR_IOMUX);
-	reg32_write(0x40048258, DDR_IOMUX);
-	reg32_write(0x4004825c, DDR_IOMUX);
-	reg32_write(0x40048260, DDR_IOMUX);	// BA 0
-	reg32_write(0x40048264, DDR_IOMUX);	// BA 1
-	reg32_write(0x40048268, DDR_IOMUX);	// BA 0
-	reg32_write(0x4004826c, DDR_IOMUX);	// CAS b
-	reg32_write(0x40048270, DDR_IOMUX);	// CKE 0
-	reg32_write(0x40048274, DDR_IOMUX1);	// CLK 0
-	reg32_write(0x40048278, DDR_IOMUX);
-	reg32_write(0x4004827c, DDR_IOMUX);
-	reg32_write(0x40048280, DDR_IOMUX);
-	reg32_write(0x40048284, DDR_IOMUX);
-	reg32_write(0x40048288, DDR_IOMUX);
-	reg32_write(0x4004828c, DDR_IOMUX);
-	reg32_write(0x40048290, DDR_IOMUX);
-	reg32_write(0x40048294, DDR_IOMUX);
-	reg32_write(0x40048298, DDR_IOMUX);
-	reg32_write(0x4004829c, DDR_IOMUX);
-	reg32_write(0x400482a0, DDR_IOMUX);
-	reg32_write(0x400482a4, DDR_IOMUX);
-	reg32_write(0x400482a8, DDR_IOMUX);
-	reg32_write(0x400482ac, DDR_IOMUX);
-	reg32_write(0x400482b0, DDR_IOMUX);
-	reg32_write(0x400482b4, DDR_IOMUX);
-	reg32_write(0x400482b8, DDR_IOMUX);
-	reg32_write(0x400482bc, DDR_IOMUX);	// DQM 1
-	reg32_write(0x400482c0, DDR_IOMUX);	// DQM 0
-	reg32_write(0x400482c4, DDR_IOMUX1);	// DQS 1
-	reg32_write(0x400482c8, DDR_IOMUX1);	// DQS 0
-	reg32_write(0x400482cc, DDR_IOMUX);	// RAS
-	reg32_write(0x400482d0, DDR_IOMUX);	// WE
-	reg32_write(0x400482d4, DDR_IOMUX);	// ODT 0
-	reg32_write(0x400482d8, DDR_IOMUX);	// ODT 1
-	reg32_write(0x400482dc, DDR_IOMUX);
-	reg32_write(0x400482e0, DDR_IOMUX);
+	__raw_writel(DDR_IOMUX, 0x4004821c);
+	__raw_writel(DDR_IOMUX, 0x40048220);
+	__raw_writel(DDR_IOMUX, 0x40048224);
+	__raw_writel(DDR_IOMUX, 0x40048228);
+	__raw_writel(DDR_IOMUX, 0x4004822c);
+	__raw_writel(DDR_IOMUX, 0x40048230);
+	__raw_writel(DDR_IOMUX, 0x40048234);
+	__raw_writel(DDR_IOMUX, 0x40048238);
+	__raw_writel(DDR_IOMUX, 0x4004823c);
+	__raw_writel(DDR_IOMUX, 0x40048240);
+	__raw_writel(DDR_IOMUX, 0x40048244);
+	__raw_writel(DDR_IOMUX, 0x40048248);
+	__raw_writel(DDR_IOMUX, 0x4004824c);
+	__raw_writel(DDR_IOMUX, 0x40048250);
+	__raw_writel(DDR_IOMUX, 0x40048254);
+	__raw_writel(DDR_IOMUX, 0x40048258);
+	__raw_writel(DDR_IOMUX, 0x4004825c);
+	__raw_writel(DDR_IOMUX, 0x40048260);	// BA 0
+	__raw_writel(DDR_IOMUX, 0x40048264);	// BA 1
+	__raw_writel(DDR_IOMUX, 0x40048268);	// BA 0
+	__raw_writel(DDR_IOMUX, 0x4004826c);	// CAS b
+	__raw_writel(DDR_IOMUX, 0x40048270);	// CKE 0
+	__raw_writel(DDR_IOMUX1, 0x40048274);	// CLK 0
+	__raw_writel(DDR_IOMUX, 0x40048278);
+	__raw_writel(DDR_IOMUX, 0x4004827c);
+	__raw_writel(DDR_IOMUX, 0x40048280);
+	__raw_writel(DDR_IOMUX, 0x40048284);
+	__raw_writel(DDR_IOMUX, 0x40048288);
+	__raw_writel(DDR_IOMUX, 0x4004828c);
+	__raw_writel(DDR_IOMUX, 0x40048290);
+	__raw_writel(DDR_IOMUX, 0x40048294);
+	__raw_writel(DDR_IOMUX, 0x40048298);
+	__raw_writel(DDR_IOMUX, 0x4004829c);
+	__raw_writel(DDR_IOMUX, 0x400482a0);
+	__raw_writel(DDR_IOMUX, 0x400482a4);
+	__raw_writel(DDR_IOMUX, 0x400482a8);
+	__raw_writel(DDR_IOMUX, 0x400482ac);
+	__raw_writel(DDR_IOMUX, 0x400482b0);
+	__raw_writel(DDR_IOMUX, 0x400482b4);
+	__raw_writel(DDR_IOMUX, 0x400482b8);
+	__raw_writel(DDR_IOMUX, 0x400482bc);	// DQM 1
+	__raw_writel(DDR_IOMUX, 0x400482c0);	// DQM 0
+	__raw_writel(DDR_IOMUX1, 0x400482c4);	// DQS 1
+	__raw_writel(DDR_IOMUX1, 0x400482c8);	// DQS 0
+	__raw_writel(DDR_IOMUX, 0x400482cc);	// RAS
+	__raw_writel(DDR_IOMUX, 0x400482d0);	// WE
+	__raw_writel(DDR_IOMUX, 0x400482d4);	// ODT 0
+	__raw_writel(DDR_IOMUX, 0x400482d8);	// ODT 1
+	__raw_writel(DDR_IOMUX, 0x400482dc);
+	__raw_writel(DDR_IOMUX, 0x400482e0);
 
        /* enable 1V5 for DDR */
 	__raw_writel(0x000000CA, IOMUXC_PAD_098);
@@ -129,36 +127,36 @@ void ddr_phy_init(void)
 #endif
   
   	// phy_dq_timing_reg freq set 0
-	reg32_write(DDR_PHY000, PHY_DQ_TIMING);
-	reg32_write(DDR_PHY016, PHY_DQ_TIMING);
-	reg32_write(DDR_PHY032, PHY_DQ_TIMING);
-	reg32_write(DDR_PHY048, PHY_DQ_TIMING);
+	__raw_writel(PHY_DQ_TIMING, DDR_PHY000);
+	__raw_writel(PHY_DQ_TIMING, DDR_PHY016);
+	__raw_writel(PHY_DQ_TIMING, DDR_PHY032);
+	__raw_writel(PHY_DQ_TIMING, DDR_PHY048);
 
   	// phy_dqs_timing_reg freq set 0
-	reg32_write(DDR_PHY001, PHY_DQS_TIMING);
-	reg32_write(DDR_PHY017, PHY_DQS_TIMING);
-	reg32_write(DDR_PHY033, PHY_DQS_TIMING);
-	reg32_write(DDR_PHY049, PHY_DQS_TIMING);
+	__raw_writel(PHY_DQS_TIMING, DDR_PHY001);
+	__raw_writel(PHY_DQS_TIMING, DDR_PHY017);
+	__raw_writel(PHY_DQS_TIMING, DDR_PHY033);
+	__raw_writel(PHY_DQS_TIMING, DDR_PHY049);
 
 	// phy_gate_lpbk_ctrl_reg freq set 0
-	reg32_write(DDR_PHY002, PHY_CTRL);	// read delay bit21:19
-	reg32_write(DDR_PHY018, PHY_CTRL);	// phase_detect_sel bit18:16
-	reg32_write(DDR_PHY034, PHY_CTRL);	// bit lpbk_ctrl bit12
-	reg32_write(DDR_PHY050, PHY_CTRL);
+	__raw_writel(PHY_CTRL, DDR_PHY002);	// read delay bit21:19
+	__raw_writel(PHY_CTRL, DDR_PHY018);	// phase_detect_sel bit18:16
+	__raw_writel(PHY_CTRL, DDR_PHY034);	// bit lpbk_ctrl bit12
+	__raw_writel(PHY_CTRL, DDR_PHY050);
 
 	// phy_dll_master_ctrl_reg freq set 0
-	reg32_write(DDR_PHY003, PHY_MASTER_CTRL);
-	reg32_write(DDR_PHY019, PHY_MASTER_CTRL);
-	reg32_write(DDR_PHY035, PHY_MASTER_CTRL);
-	reg32_write(DDR_PHY051, PHY_MASTER_CTRL);
+	__raw_writel(PHY_MASTER_CTRL, DDR_PHY003);
+	__raw_writel(PHY_MASTER_CTRL, DDR_PHY019);
+	__raw_writel(PHY_MASTER_CTRL, DDR_PHY035);
+	__raw_writel(PHY_MASTER_CTRL, DDR_PHY051);
 
 	// phy_dll_slave_ctrl_reg freq set 0
-	reg32_write(DDR_PHY004, PHY_SLAVE_CTRL);
-	reg32_write(DDR_PHY020, PHY_SLAVE_CTRL);
-	reg32_write(DDR_PHY036, PHY_SLAVE_CTRL);
-	reg32_write(DDR_PHY052, PHY_SLAVE_CTRL);
+	__raw_writel(PHY_SLAVE_CTRL, DDR_PHY004);
+	__raw_writel(PHY_SLAVE_CTRL, DDR_PHY020);
+	__raw_writel(PHY_SLAVE_CTRL, DDR_PHY036);
+	__raw_writel(PHY_SLAVE_CTRL, DDR_PHY052);
 
-	reg32_write(DDR_PHY050, 0x00001105);
+	__raw_writel(0x00001105, DDR_PHY050);
 }
 
 unsigned long ddr_ctrl_init(void)
@@ -173,50 +171,50 @@ unsigned long ddr_ctrl_init(void)
 	//
 	// Dram Device Parameters
 	//
-	reg32_write(DDR_CR000, 0x00000600);	// LPDDR2 or DDR3
-	reg32_write(DDR_CR002, 0x00000020);	// TINIT F0
+	__raw_writel(0x00000600, DDR_CR000);	// LPDDR2 or DDR3
+	__raw_writel(0x00000020, DDR_CR002);	// TINIT F0
 	// cold boot - 1ms??? 0x61a80
-	reg32_write(DDR_CR010, 0x0000007c);	// reset during power on
+	__raw_writel(0x0000007c, DDR_CR010);	// reset during power on
 						// warm boot - 200ns
-	reg32_write(DDR_CR011, 0x00013880);	// 500us - 10ns
-	reg32_write(DDR_CR012, 0x0000050c);	// CASLAT_LIN, WRLAT
-	reg32_write(DDR_CR013, 0x15040404);	// trc, trrd, tccd, tbst_int_interval
-	reg32_write(DDR_CR014, 0x1406040F);	// tfaw, trp, twtr, tras_min
-	reg32_write(DDR_CR016, 0x04040000);	// tmrd, trtp
-	reg32_write(DDR_CR017, 0x006db00c);	// tras_max, tmod
-	reg32_write(DDR_CR018, 0x00000403);	// tckesr, tcke
+	__raw_writel(0x00013880, DDR_CR011);	// 500us - 10ns
+	__raw_writel(0x0000050c, DDR_CR012);	// CASLAT_LIN, WRLAT
+	__raw_writel(0x15040404, DDR_CR013);	// trc, trrd, tccd, tbst_int_interval
+	__raw_writel(0x1406040F, DDR_CR014);	// tfaw, trp, twtr, tras_min
+	__raw_writel(0x04040000, DDR_CR016);	// tmrd, trtp
+	__raw_writel(0x006db00c, DDR_CR017);	// tras_max, tmod
+	__raw_writel(0x00000403, DDR_CR018);	// tckesr, tcke
 
-	reg32_write(DDR_CR020, 0x01000403);	// ap, writeinterp, tckesr_f1, tcke_f1
+	__raw_writel(0x01000403, DDR_CR020);	// ap, writeinterp, tckesr_f1, tcke_f1
 
-//	reg32_write(DDR_CR021, 0x06060101);	// twr_int, trcd_int, tras_lockout, ccAP
-	reg32_write(DDR_CR021, 0x00060000);
+//	__raw_writel(0x06060101, DDR_CR021);	// twr_int, trcd_int, tras_lockout, ccAP
+	__raw_writel(0x00060000, DDR_CR021);
 
-	reg32_write(DDR_CR022, 0x000b0000);	// tdal
-	reg32_write(DDR_CR023, 0x03000200);	// bstlen, tmrr - lpddr2, tdll
-	reg32_write(DDR_CR024, 0x00000006);	// addr_mirror, reg_dimm, trp_ab
-	reg32_write(DDR_CR025, 0x00010000);	// tref_enable, auto_refresh, arefresh
-	reg32_write(DDR_CR026, 0x0c28002c);	// tref, trfc
-	reg32_write(DDR_CR028, 0x00000005);	// tref_interval fixed at 5
-	reg32_write(DDR_CR029, 0x00000003);	// tpdex_f0
+	__raw_writel(0x000b0000, DDR_CR022);	// tdal
+	__raw_writel(0x03000200, DDR_CR023);	// bstlen, tmrr - lpddr2, tdll
+	__raw_writel(0x00000006, DDR_CR024);	// addr_mirror, reg_dimm, trp_ab
+	__raw_writel(0x00010000, DDR_CR025);	// tref_enable, auto_refresh, arefresh
+	__raw_writel(0x0c28002c, DDR_CR026);	// tref, trfc
+	__raw_writel(0x00000005, DDR_CR028);	// tref_interval fixed at 5
+	__raw_writel(0x00000003, DDR_CR029);	// tpdex_f0
 
-	reg32_write(DDR_CR030, 0x0000000a);	// txpdll
-	reg32_write(DDR_CR031, 0x00440200);	// txsnr, txsr
-	reg32_write(DDR_CR033, 0x00010000);	// cke_dly, en_quick_srefresh,
+	__raw_writel(0x0000000a, DDR_CR030);	// txpdll
+	__raw_writel(0x00440200, DDR_CR031);	// txsnr, txsr
+	__raw_writel(0x00010000, DDR_CR033);	// cke_dly, en_quick_srefresh,
 						// srefresh_exit_no_refresh,
 						// pwr, srefresh_exit
-	reg32_write(DDR_CR034, 0x00050500);	// cksrx_f0,
+	__raw_writel(0x00050500, DDR_CR034);	// cksrx_f0,
 						// cksre_f0, lowpwr_ref_en
 
 	//
 	// Frequency change
 	//
-	reg32_write(DDR_CR038, 0x00000100);	// freq change...
-	reg32_write(DDR_CR039, 0x04001002);	// PHY_INI: com, sta, freq_ch_dll_off
+	__raw_writel(0x00000100, DDR_CR038);	// freq change...
+	__raw_writel(0x04001002, DDR_CR039);	// PHY_INI: com, sta, freq_ch_dll_off
 
-	reg32_write(DDR_CR041, 0x00000001);	// 15.02 - allow dfi_init_start
-	reg32_write(DDR_CR045, 0x00000000);	// wrmd
-	reg32_write(DDR_CR046, 0x00000000);	// rmd
-	reg32_write(DDR_CR047, 0x00000000);	// REF_PER_AUTO_TEMPCHK: LPDDR2 set to 2, else 0
+	__raw_writel(0x00000001, DDR_CR041);	// 15.02 - allow dfi_init_start
+	__raw_writel(0x00000000, DDR_CR045);	// wrmd
+	__raw_writel(0x00000000, DDR_CR046);	// rmd
+	__raw_writel(0x00000000, DDR_CR047);	// REF_PER_AUTO_TEMPCHK: LPDDR2 set to 2, else 0
 
 	//
 	// DRAM device Mode registers
@@ -270,120 +268,120 @@ unsigned long ddr_ctrl_init(void)
 #define MR2_DODT_RZQ_2	0x0400	// RZQ / 2
 #define MR2	(MR2_CWL_5CK | MR2_DODT_DIS)
 
-	reg32_write(DDR_CR048, ((MR1 << 16) | MR));	// mr0, ddr3 burst of 8 only
+	__raw_writel(((MR1 << 16) | MR), DDR_CR048);	// mr0, ddr3 burst of 8 only
 						// mr1, if freq < 125, dll_dis = 1, rtt = 0
 						// if freq > 125, dll_dis = 0, rtt = 3
-	reg32_write(DDR_CR049, MR2);	// mr0_f1_0 & mr2_f0_0
+	__raw_writel(MR2, DDR_CR049);	// mr0_f1_0 & mr2_f0_0
 
-	reg32_write(DDR_CR051, 0x00000000);	// mr3 & mrsingle_data
-	reg32_write(DDR_CR052, 0x00000000);	// mr17, mr16
+	__raw_writel(0x00000000, DDR_CR051);	// mr3 & mrsingle_data
+	__raw_writel(0x00000000, DDR_CR052);	// mr17, mr16
 
 	//
 	// ECC
 	//
-	reg32_write(DDR_CR057, 0x00000000);	// ctrl_raw
+	__raw_writel(0x00000000, DDR_CR057);	// ctrl_raw
 						// ctrl_raw, if DDR3, set 3, else 0
-	reg32_write(DDR_CR058, 0x00000000);
+	__raw_writel(0x00000000, DDR_CR058);
 
 	//
 	// ZQ stuff
 	//
-	reg32_write(DDR_CR066, 0x01000200);	// zqcl, zqinit
-	reg32_write(DDR_CR067, 0x02000040);	// zqcs
-	reg32_write(DDR_CR069, 0x00000200);	// zq_on_sref_exit, qz_req
+	__raw_writel(0x01000200, DDR_CR066);	// zqcl, zqinit
+	__raw_writel(0x02000040, DDR_CR067);	// zqcs
+	__raw_writel(0x00000200, DDR_CR069);	// zq_on_sref_exit, qz_req
 
-	reg32_write(DDR_CR070, 0x00000040);	// ref_per_zq
-	reg32_write(DDR_CR071, 0x00000000);	// zqreset, ddr3 set to 0
-	reg32_write(DDR_CR072, 0x01000000);	// zqcs_rotate, no_zq_init, zqreset_f1
+	__raw_writel(0x00000040, DDR_CR070);	// ref_per_zq
+	__raw_writel(0x00000000, DDR_CR071);	// zqreset, ddr3 set to 0
+	__raw_writel(0x01000000, DDR_CR072);	// zqcs_rotate, no_zq_init, zqreset_f1
 
 	//
 	// DRAM controller misc
 	//
-//	reg32_write(DDR_CR073, 0x0a020301);	// arebit, col_diff, row_diff, bank_diff
+//	__raw_writel(0x0a020301, DDR_CR073);	// arebit, col_diff, row_diff, bank_diff
 
-	reg32_write(DDR_CR073, 0x0a010300);	// arebit, col_diff, row_diff, bank_diff
+	__raw_writel(0x0a010300, DDR_CR073);	// arebit, col_diff, row_diff, bank_diff
 
-	reg32_write(DDR_CR074, 0x0101ffff);	// bank_split, addr_cmp_en, cmd/age cnt
-	reg32_write(DDR_CR075, 0x01010101);	// rw same pg, rw same en, pri en, plen
-	reg32_write(DDR_CR076, 0x03030101);	// #q_entries_act_dis, (#cmdqueues
+	__raw_writel(0x0101ffff, DDR_CR074);	// bank_split, addr_cmp_en, cmd/age cnt
+	__raw_writel(0x01010101, DDR_CR075);	// rw same pg, rw same en, pri en, plen
+	__raw_writel(0x03030101, DDR_CR076);	// #q_entries_act_dis, (#cmdqueues
 						// dis_rw_grp_w_bnk_conflict
 						// w2r_split_en, cs_same_en
-       	reg32_write(DDR_CR077, 0x01000101);	// cs_map, inhibit_dram_cmd, dis_interleave, swen //by devendra
-	reg32_write(DDR_CR078, 0x0000000c);	// qfull, lpddr2_s4, reduc, burst_on_fly
-	reg32_write(DDR_CR079, 0x01000000);	// ctrlupd_req_per aref en, ctrlupd_req
+       	__raw_writel(0x01000101, DDR_CR077);	// cs_map, inhibit_dram_cmd, dis_interleave, swen //by devendra
+	__raw_writel(0x0000000c, DDR_CR078);	// qfull, lpddr2_s4, reduc, burst_on_fly
+	__raw_writel(0x01000000, DDR_CR079);	// ctrlupd_req_per aref en, ctrlupd_req
 						// ctrller busy, in_ord_accept
 
 	//
 	// ODT
 	//
-	reg32_write(DDR_CR087, 0x01010000);	// odt: wr_map_cs, rd_map_cs, port_data_err_id
-	reg32_write(DDR_CR088, 0x00040000);	// todtl_2cmd
+	__raw_writel(0x01010000, DDR_CR087);	// odt: wr_map_cs, rd_map_cs, port_data_err_id
+	__raw_writel(0x00040000, DDR_CR088);	// todtl_2cmd
 						// todtl_2cmd = odtl_off = CWL + AL - 2ck
-	reg32_write(DDR_CR089, 0x00000002);	// add_odt stuff
-	reg32_write(DDR_CR091, 0x00020000);	
-	reg32_write(DDR_CR092, 0x00000000);	// tdqsck_min, _max, w2w_smcsdl
+	__raw_writel(0x00000002, DDR_CR089);	// add_odt stuff
+	__raw_writel(0x00020000, DDR_CR091);	
+	__raw_writel(0x00000000, DDR_CR092);	// tdqsck_min, _max, w2w_smcsdl
 
-	reg32_write(DDR_CR096, 0x00002819);	// wlmrd, wldqsen
+	__raw_writel(0x00002819, DDR_CR096);	// wlmrd, wldqsen
 
 #if 0
-	reg32_write(DDR_CR105, 0x00020200);
-	reg32_write(DDR_CR106, 0x02020000);
-	reg32_write(DDR_CR110, 0x00000202);
-	reg32_write(DDR_CR114, 0x00202000);
-	reg32_write(DDR_CR115, 0x02020000);
+	__raw_writel(0x00020200, DDR_CR105);
+	__raw_writel(0x02020000, DDR_CR106);
+	__raw_writel(0x00000202, DDR_CR110);
+	__raw_writel(0x00202000, DDR_CR114);
+	__raw_writel(0x02020000, DDR_CR115);
 #else
-	reg32_write(DDR_CR105, 0x00202000);
-	reg32_write(DDR_CR106, 0x20200000);
-	reg32_write(DDR_CR110, 0x00002020);
-	reg32_write(DDR_CR114, 0x00202000);
-	reg32_write(DDR_CR115, 0x20200000);
+	__raw_writel(0x00202000, DDR_CR105);
+	__raw_writel(0x20200000, DDR_CR106);
+	__raw_writel(0x00002020, DDR_CR110);
+	__raw_writel(0x00202000, DDR_CR114);
+	__raw_writel(0x20200000, DDR_CR115);
 #endif
 	
 	//
 	// AXI ports
 	//
-//	reg32_write(DDR_CR117, 0x00000101);	// FIFO type (0-async, 1-2:1, 2-1:2, 3- sync, w_pri, r_prii
-	reg32_write(DDR_CR117, 0x00020101);	// FIFO type (0-async, 1-2:1, 2-1:2, 3- sync, w_pri, r_prii
+//	__raw_writel(0x00000101, DDR_CR117);	// FIFO type (0-async, 1-2:1, 2-1:2, 3- sync, w_pri, r_prii
+	__raw_writel(0x00020101, DDR_CR117);	// FIFO type (0-async, 1-2:1, 2-1:2, 3- sync, w_pri, r_prii
 
-	reg32_write(DDR_CR118, 0x01010000);	// w_pri, rpri, en
+	__raw_writel(0x01010000, DDR_CR118);	// w_pri, rpri, en
 
-//	reg32_write(DDR_CR119, 0x00000000);	// fifo_type
-	reg32_write(DDR_CR119, 0x00000002);	// fifo_type/
+//	__raw_writel(0x00000000, DDR_CR119);	// fifo_type
+	__raw_writel(0x00000002, DDR_CR119);	// fifo_type/
 
-	reg32_write(DDR_CR120, 0x02020000);
-	reg32_write(DDR_CR121, 0x00000202);	// round robin port ordering
-	reg32_write(DDR_CR122, 0x01010064);
-	reg32_write(DDR_CR123, 0x00010101);
-	reg32_write(DDR_CR124, 0x00000064);
+	__raw_writel(0x02020000, DDR_CR120);
+	__raw_writel(0x00000202, DDR_CR121);	// round robin port ordering
+	__raw_writel(0x01010064, DDR_CR122);
+	__raw_writel(0x00010101, DDR_CR123);
+	__raw_writel(0x00000064, DDR_CR124);
 
 	//
 	// TDFI
 	//
-	reg32_write(DDR_CR125, 0x00000000);	// dll_rst_adj_dly, dll_rst_delay
-	reg32_write(DDR_CR126, 0x00000b00);	// phy_rdlat
-	reg32_write(DDR_CR127, 0x00000000);	// dram_ck_dis
-//	reg32_write(DDR_CR131, 0x00003cc8);	// tdfi_ctrlupd_interval_f0
-	reg32_write(DDR_CR131, 0);	// tdfi_ctrlupd_interval_f0
+	__raw_writel(0x00000000, DDR_CR125);	// dll_rst_adj_dly, dll_rst_delay
+	__raw_writel(0x00000b00, DDR_CR126);	// phy_rdlat
+	__raw_writel(0x00000000, DDR_CR127);	// dram_ck_dis
+//	__raw_writel(0x00003cc8, DDR_CR131);	// tdfi_ctrlupd_interval_f0
+	__raw_writel(0, DDR_CR131);	// tdfi_ctrlupd_interval_f0
 #define WRADJ	0x00000500
 #define RDADJ	0x00000006
 #define ADJLAT	(WRADJ | RDADJ)
-	reg32_write(DDR_CR132, ADJLAT);	// wrlat, rdlat 15.02
-	reg32_write(DDR_CR137, 0x00020000);	// Phyctl_dl
-	reg32_write(DDR_CR139, 0x04070303);
+	__raw_writel(ADJLAT, DDR_CR132);	// wrlat, rdlat 15.02
+	__raw_writel(0x00020000, DDR_CR137);	// Phyctl_dl
+	__raw_writel(0x04070303, DDR_CR139);
 
-//	reg32_write(DDR_CR136, 0x000012e3);	// tdfi_ctrlupd_interval_f1
-	reg32_write(DDR_CR136, 0);	// tdfi_ctrlupd_interval_f1
+//	__raw_writel(0x000012e3, DDR_CR136);	// tdfi_ctrlupd_interval_f1
+	__raw_writel(0, DDR_CR136);	// tdfi_ctrlupd_interval_f1
 
-	reg32_write(DDR_CR154, 0x68200000);	// pad_zq: _early_cmp_en_timer, _mode, _hw_for, _cmp_out_smp
-//	reg32_write(DDR_CR155, 0x00000202);	// pad: _ibe1, ibe0, pad_ibe: _sel1, _sel0,
-	reg32_write(DDR_CR155, 0x00000212);	// pad: _ibe1, ibe0, pad_ibe: _sel1, _sel0,
+	__raw_writel(0x68200000, DDR_CR154);	// pad_zq: _early_cmp_en_timer, _mode, _hw_for, _cmp_out_smp
+//	__raw_writel(0x00000202, DDR_CR155);	// pad: _ibe1, ibe0, pad_ibe: _sel1, _sel0,
+	__raw_writel(0x00000212, DDR_CR155);	// pad: _ibe1, ibe0, pad_ibe: _sel1, _sel0,
 						// axi_awcache, axi_cobuf, pad_odt: bate0, byte1
-	reg32_write(DDR_CR158, 0x00000006);	// twr
-	reg32_write(DDR_CR159, 0x00000006);	// todth
+	__raw_writel(0x00000006, DDR_CR158);	// twr
+	__raw_writel(0x00000006, DDR_CR159);	// todth
 
         ddr_phy_init();
-        reg32_write(DDR_CR082, 0x1FFFFFFF);
-	reg32_write(DDR_CR000, 0x00000601);	// LPDDR2 or DDR3, start
+        __raw_writel(0x1FFFFFFF, DDR_CR082);
+	__raw_writel(0x00000601, DDR_CR000);	// LPDDR2 or DDR3, start
 
 
 	i = 5000;
