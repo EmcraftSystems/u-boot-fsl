cf877a050f041bb104587c95f929e3300336b094
Yuri Tikhonov <yur@emcraft.com>
2017-10-09 18:40:59 +0300

    RM-1960 mxrt105x: use `-mno-fdpic` option for compilation
    Signed-off-by: Yuri Tikhonov <yur@emcraft.com>

---
+++ arch/arm/mach-imxrt105x/cpu.c
@@ -0,0 +1,34 @@
+/*
+ * (C) Copyright 2017 Emcraft Systems
+ * Dmitry Konyshev <probables@emcraft.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <asm/armv7m_mpu.h>
+
+int arch_cpu_init(void)
+{
+	struct mpu_region_config imxrt1050_region_config[] = {
+		{ 0x00000000, REGION_0, XN_DIS, PRIV_RW_USR_RW,
+		  STRONG_ORDER, REGION_4GB },
+		{ PHYS_SDRAM, REGION_1, XN_DIS, PRIV_RW_USR_RW,
+		  O_I_WB_RD_WR_ALLOC, (ffs(PHYS_SDRAM_SIZE) - 2) },
+		{ DMAMEM_BASE,
+		  REGION_2, XN_DIS, PRIV_RW_USR_RW,
+		  STRONG_ORDER, (ffs(DMAMEM_SZ_ALL) - 2) },
+	};
+	int i;
+
+	/*
+	 * Configure the memory protection unit (MPU) to allow full access to
+	 * the whole 4GB address space.
+	 */
+	disable_mpu();
+	for (i = 0; i < ARRAY_SIZE(imxrt1050_region_config); i++)
+		mpu_config(&imxrt1050_region_config[i]);
+	enable_mpu();
+
+	return 0;
+}
